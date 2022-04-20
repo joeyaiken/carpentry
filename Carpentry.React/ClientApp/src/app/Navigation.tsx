94c7de296bc1fd131fe76ae7b134652e8f05a7d3
Joey Aiken <jaiken25@gmail.com>
2022-04-19 22:41:04 -0400

    Some more-clean components

---
+++ Carpentry.React/ClientApp/src/app/Navigation.tsx
@@ -0,0 +1,24 @@
+﻿import React from "react"
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
+import styles from './App.module.css'
+import {Link} from "react-router-dom";
+
+export const Navigation = (): JSX.Element => {
+  return (
+    <AppBar id="app-nav-menu" position="static">
+      <Toolbar>
+        <Link component={IconButton} color="inherit" to={'/'}>
+          <Menu />
+        </Link>
+        <Typography variant="h5" className={styles.flexSection}>
+          Carpentry
+        </Typography>
+        <Link component={Button} className="nav-button" to={'/decks'}>Decks</Link>
+        <Link component={Button} className="nav-button" to={'/inventory'}>Inventory</Link>
+        <Link component={Button} className="nav-button" to={'/settings/sets'}>Settings</Link>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
---
+++ Carpentry.React/ClientApp/src/features/decks/deck-list/DeckList.tsx
@@ -0,0 +1,60 @@
+﻿import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {loadDeckOverviews} from "../decksDataSlice";
+import {Avatar, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import {RootState} from "../../../app/store";
+
+const DeckListRow = (props: {deckId: number}) => {
+  const deck: DeckOverviewDto = useSelector((state: RootState) =>
+    state.decks.data.overviews.decksById[props.deckId]);
+
+  const manaIcon = (type: string): JSX.Element => {
+    return(<Avatar key={type} style={{height:"24px", width:"24px", display:"inline-flex", verticalAlign: "middle"}} src={`/img/${type}.svg`} />)
+  }
+
+  return (
+    <TableRow key={deck.id}>
+      <TableCell>
+        <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+      </TableCell>
+      <TableCell>{deck.format}</TableCell>
+      <TableCell>
+        {
+          deck.colors.map(color => manaIcon(color))
+        }
+      </TableCell>
+      <TableCell>{deck.validationIssues}</TableCell>
+    </TableRow>
+  )
+}
+
+export const DeckList = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const shouldLoad = useSelector((state: RootState) => 
+    !state.decks.data.overviews.isLoading && !state.decks.data.overviews.isInitialized);
+
+  const deckIds: number[] = useSelector((state: RootState) => state.decks.data.overviews.deckIds);
+  
+  useEffect(() => {
+    if(shouldLoad) dispatch(loadDeckOverviews());
+  });
+
+  return (
+    <React.Fragment>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Format</TableCell>
+            <TableCell>Colors</TableCell>
+            <TableCell>Validity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { deckIds.map(id => <DeckListRow key={id} deckId={id} />) }
+        </TableBody>
+      </Table>
+    </React.Fragment>
+  )
+}
